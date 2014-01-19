ee1843e90d22007c0bc6cfea3d282aa378ec42ff
Christophe Coevoet <stof@notk.org>
2014-01-19 21:24:02 +0100

    Removed the getter for the SelectorsHandler in the Session
    This allows getting rid of the SelectorsHandler as a collaborator of the
    Session class.
    People needing to access the SelectorsHandler directly should instantiate
    the ElementFinder explicitly with a SelectorsHandler, and keep a reference
    to it.

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -10,7 +10,6 @@ class SessionTest extends \PHPUnit_Framework_TestCase
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
     private $driver;
-    private $selectorsHandler;
 
     /**
      * Session.
@@ -21,9 +20,8 @@ class SessionTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->driver           = $this->getMockBuilder('Behat\Mink\Driver\DriverInterface')->getMock();
-        $this->selectorsHandler = $this->getMockBuilder('Behat\Mink\Selector\SelectorsHandler')->getMock();
-        $this->session  = new Session($this->driver, $this->selectorsHandler);
+        $this->driver = $this->getMockBuilder('Behat\Mink\Driver\DriverInterface')->getMock();
+        $this->session = new Session($this->driver);
     }
 
     public function testGetDriver()
@@ -34,11 +32,6 @@ class SessionTest extends \PHPUnit_Framework_TestCase
     public function testGetPage()
     {
         $this->assertInstanceOf('Behat\Mink\Element\DocumentElement', $this->session->getPage());
-    }
-
-    public function testGetSelectorsHandler()
-    {
-        $this->assertSame($this->selectorsHandler, $this->session->getSelectorsHandler());
     }
 
     public function testIsStarted()
